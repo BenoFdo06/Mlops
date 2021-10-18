2b1245db795008b2138b6caae9965f9df719f665
BenoFdo06 <88475477+BenoFdo06@users.noreply.github.com>
2021-10-18 16:12:27 +0530

    Update bank_default_app.py
    Commit to check auto build

--- bank_default_app.py
+++ bank_default_app.py
@@ -27,7 +27,7 @@ classifier=pickle.load(pickle_in)
 
 @app.route('/')
 def welcome():
-    return "Welcome All"
+    return "Welcome All "
 
 @app.route('/predict',methods=["Get"])
 def predict_note_authentication():
